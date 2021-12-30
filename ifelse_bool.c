e8ca119aafbe31f00b4b19b1a79b208451840175
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 12:21:20 +0530

    Create ifelse_bool.c

---
+++ ifelse_bool.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<bool.h>
+
+int main()
+	{
+		int a;
+		printf("Enter the Value of a = ");
+		fflush(stdout);
+		scanf("%d",&a);
+		if (a)
+			{
+				printf("\n True");
+			}
+		else
+			{
+				printf("\n False");
+			}
+	}
