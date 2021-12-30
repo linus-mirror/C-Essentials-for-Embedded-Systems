3e8997badecc276b67cb711da83d4595e3d761ab
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 12:14:11 +0530

    Create char_arth.c

---
+++ char_arth.c
@@ -0,0 +1,13 @@
+//Arithmetic Operation for char datatype
+
+#include<stdio.h>
+
+int main()
+{
+	char a = 'A';
+
+	printf(" Value of %c is %d : ",a,a);
+	a++;
+	printf("\n Value of %c is %d after increment.",a,a);
+
+}
