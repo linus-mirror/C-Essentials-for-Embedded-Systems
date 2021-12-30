036c0be6d49b43608a3290ba9480a17dda955c45
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 11:35:56 +0530

    Create data_type2.c

---
+++ data_type2.c
@@ -0,0 +1,16 @@
+//Data Types - Printing Character and printing its corresponding Integer value.
+
+int main()
+{
+	int x = 66;
+	char p = 'P';
+	float y=73.4;
+	int z = -67;
+
+	printf("\n Integer and Character value of x = %d --> %c",x,x);
+	printf("\n Integer and Character value of p = %c --> %c",p,p);
+	printf("\n Integer and Hexa value of x = %d --> %x",x,x);
+	printf("\n Integer and Octal value of x = %d --> %o",x,x);
+	printf("\n Integer and Float value of y = %f --> %d",y,y);
+	printf("\n Integer and unsigned value of z = %d --> %u",z,z);
+}
