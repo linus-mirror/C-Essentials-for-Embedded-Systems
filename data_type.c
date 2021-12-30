9d4fa349cd8b4416ae2d21da29967af5612f1a65
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 11:34:48 +0530

    Create data_type.c

---
+++ data_type.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int a = -65;
+	float b = 65;
+	int c = 65;
+	int d = -65;
+	int e = -123455678.91234556789;
+
+	printf("\n int x = %d",a);
+	printf("\n float x = %f",b);
+	printf("\n char x = %c",c);
+	printf("\n unsigned x = %u",d);
+	printf("\n long double x = %ld",e);
+
+	return 0;
+}
