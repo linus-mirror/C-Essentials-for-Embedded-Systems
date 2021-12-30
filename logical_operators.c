d98eb1d7651e25abf59a9b0b1d4254d0c5fcb316
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 12:04:20 +0530

    Create logical_operators.c

---
+++ logical_operators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 8;
+	int b = 2;
+
+	printf("\n a AND b = %d",a&b);
+	printf("\n a OR b = %d",a|b);
+	printf("\n NOT a = %d", ~a);
+	printf("\n a XOR b = %d",a^b);
+	return 0;
+}
