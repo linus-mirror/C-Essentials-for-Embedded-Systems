aa3adf5825215e5ba987e5cd1d57af13794e60bd
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 11:41:56 +0530

    Create s_printf.c

---
+++ s_printf.c
@@ -0,0 +1,16 @@
+//Character count using sprintf
+
+#include<stdio.h>
+
+int main()
+{
+	char *s = "Hi C Language.";
+	char a[100];
+	int x=7, y=6;
+
+	printf(s);
+	printf("\n %s",s); // Proper way of printing a variable
+
+	sprintf(a,"\nx = %d, y = %d\n", x, y); // Variable "a" get assigned with the data "STRING" entered for x and y
+	printf("\n%s",a);
+}
