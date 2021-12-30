c4d230a6469b3040b5300b661db6cfb61a021337
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 11:53:37 +0530

    Create sizeof_datatype.c

---
+++ sizeof_datatype.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdint.h>
+
+int main()
+{
+	size_t s;
+	unsigned char A;
+	int I;
+	int N = -1;
+	signed char B;
+	short int P;
+	long int Q;
+	long long int R;
+	float F;
+	double X;
+
+	B = 65.35; // Assigning a float value to a character.
+	printf("\n Size of signed char B = %c",B); // Prints Char = 65 i,e; "A" and floating number gets ignored.
+  
+  //Size of stdint datatypes.
+  
+	printf(" Size of unsigned char A = %d Bytes.",sizeof(A));
+	printf("\n Size of signed int I = %d Bytes.",sizeof(I));
+	printf("\n Size of signed char B = %d Bytes.",sizeof(B));
+	printf("\n Size of short int P = %d Bytes.",sizeof(P));
+	printf("\n Size of long int Q = %d Bytes.",sizeof(Q));
+	printf("\n Size of long long int R = %d Bytes.",sizeof(R));
+	printf("\n Size of float F = %d Bytes.",sizeof(F));
+	printf("\n Size of double X = %d Bytes.",sizeof(X));
+	printf("\n Size of unsigned N = %d Bytes.",sizeof(N));
+	printf("\n Size of size_t s = %d Bytes.",sizeof(s)); // Included in <stdint.h> library
+
+	return 0;
+}
