b9b3684cb94c4cbaf2ea4f202c1d9655ba653ae4
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 11:37:13 +0530

    Create character_count.c

---
+++ character_count.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	int x = 5, y=6;
+
+	int charcount = 0;
+	charcount = printf("\n Value of x = %d and y = %d",x,y); // Return value of a printf Statement.
+	printf("\n Total number of characters printed in Console = %d",charcount);
+}
