0a89756fbbf82cecf5d4d3f27fe54acbe7afbf3a
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 12:44:40 +0530

    Create switch_case.c

---
+++ switch_case.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+	int x;
+
+	printf(" Enter the value of x = ");
+	fflush(stdout);
+	scanf("%d",&x);
+
+	if(x <= 3)
+	{
+		switch (x) {
+		case 1:
+			printf("\n Value of x = %d",x);
+			break;
+		case 2:
+			printf("\n Value of x = %d",x);
+			break;
+		case 3:
+			printf("\n Value of x = %d",x);
+			break;
+		default:
+			printf("\n Switch 0 doesn't exist");
+			break;
+		}
+
+	}
+	else
+	{
+		printf("\n Invalid Switch case.");
+	}
+}
