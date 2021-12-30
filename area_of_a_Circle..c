73c58e7e3f2ef85d3dd463a59eca966894dd173d
Sunil Kumar S <80781652+linus-mirror@users.noreply.github.com>
2021-12-30 13:25:23 +0530

    Create area_of_a_Circle..c

---
+++ area_of_a_Circle..c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void disp(void);
+float square(float);
+
+int main()
+{
+	disp();
+	return 0;
+}
+
+void disp()
+{
+	float r = 2.5;
+	float r_sqr;
+	float AreaOfCircle;
+	float pi = 3.14;
+
+	printf("Enter the radius of the Circle = ");
+	fflush(stdout);
+	scanf("%f",&r);
+
+	r_sqr = square(r);
+
+	AreaOfCircle = (pi * (r_sqr));
+	printf("Area of a circle = %f",AreaOfCircle);
+}
+
+float square(float x)
+{
+	float sqr;
+
+	sqr= x * x;
+	return sqr;
+}
